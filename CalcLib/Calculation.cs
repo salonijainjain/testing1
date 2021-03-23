3969d2dea0ec51b82fa6d6a5001dc3ae48976dc8
salonijainjain <Salonijain010799@gmail.com>
2021-03-23 15:40:28 +0530

    First commit

---
+++ CalcLib/Calculation.cs
@@ -0,0 +1,16 @@
+﻿namespace CalcLib
+{
+    internal class Calculation
+    {
+        internal int Square(int n1,int n2)
+        {
+            int result1 = n1 * n2;
+            return result1;
+        }
+        internal int Cube(int n1,int n2,int n3)
+        {
+            int result2 = n1 * n2 * n3;
+            return result2;
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+          CalcLib.Calculation objc=new CalcLib.Calculation
+            int n1 = 5;
+            int n2 = 5;
+            int n3 = 5;
+            int result1 = n1 * n2;
+            int result2 = n1 * n2 * n3;
+
+            Assert.AreEqual(result1, objc.Square(n1, n2));
+            Assert.AreEqual(result2, objc.Cube(n1, n2,n3));
+
+        }
+    }
+}
